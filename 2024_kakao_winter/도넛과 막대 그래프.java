9d461293a3ea56edc213de0734b7b1715de9857e
unknown <choihs0457@gmail.com>
2025-03-13 15:38:24 +0900

    2025-03-13

---
+++ 2024_kakao_winter/도넛과 막대 그래프.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class GiftPrediction {
+    public static void main(String[] args) {
+        int[][] edges1 = {{2, 3}, {4, 3}, {1, 1}, {2, 1}};
+        System.out.println(solution(edges1));
+
+        int[][] edges2 = {{4, 11}, {1, 12}, {8, 3}, {12, 7}, {4, 2}, {7, 11}, {4, 8}, {9, 6}, {10, 11}, {6, 10}, {3, 5}, {11, 1}, {5, 3}, {11, 9}, {3, 8}}
+        ;
+        System.out.println(solution(edges2));
+    }
+
+    public static int[] solution(int[][] edges) {
+        int[] answer = {};
+        
+        return answer;
+    }
+}
