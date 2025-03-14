08392630bab34ef46c9df9ff445ef0ecde95c624
unknown <choihs0457@gmail.com>
2025-03-14 13:15:15 +0900

    2025.03.14

--- 2024_kakao_winter/도넛과 막대 그래프.java
+++ 2024_kakao_winter/도넛과 막대 그래프.java
@@ -1,18 +1,49 @@
 import java.util.*;
 
-class GiftPrediction {
-    public static void main(String[] args) {
-        int[][] edges1 = {{2, 3}, {4, 3}, {1, 1}, {2, 1}};
-        System.out.println(solution(edges1));
-
-        int[][] edges2 = {{4, 11}, {1, 12}, {8, 3}, {12, 7}, {4, 2}, {7, 11}, {4, 8}, {9, 6}, {10, 11}, {6, 10}, {3, 5}, {11, 1}, {5, 3}, {11, 9}, {3, 8}}
-        ;
-        System.out.println(solution(edges2));
-    }
+class Problem {
 
     public static int[] solution(int[][] edges) {
-        int[] answer = {};
-        
+        int[] answer = new int[4];
+
+        Map<Integer, int[]> exchangeCnts = new HashMap<>();
+
+        for (int[] edge : edges) {
+            int a = edge[0], b = edge[1];
+
+            exchangeCnts.putIfAbsent(a, new int[]{0, 0});
+            exchangeCnts.putIfAbsent(b, new int[]{0, 0});
+
+            exchangeCnts.get(a)[0] += 1;
+            exchangeCnts.get(b)[1] += 1;
+        }
+
+        for (Map.Entry<Integer, int[]> entry : exchangeCnts.entrySet()) {
+            int key = entry.getKey();
+            int[] exchangeCnt = entry.getValue();
+
+            if (exchangeCnt[0] >= 2 && exchangeCnt[1] == 0) {
+                answer[0] = key;
+            } else if (exchangeCnt[0] == 0 && exchangeCnt[1] > 0) {
+                answer[2] += 1;
+            } else if (exchangeCnt[0] >= 2 && exchangeCnt[1] >= 2) {
+                answer[3] += 1;
+            }
+        }
+
+        answer[1] = exchangeCnts.get(answer[0])[0] - answer[2] - answer[3];
+
         return answer;
+    }
+
+    public static void main(String[] args) {
+
+        int[][] edges1 = {{2, 3}, {4, 3}, {1, 1}, {2, 1}};
+        System.out.println(Arrays.toString(solution(edges1)));
+
+        int[][] edges2 = {
+            {4, 11}, {1, 12}, {8, 3}, {12, 7}, {4, 2}, {7, 11}, {4, 8}, {9, 6}, {10, 11},
+            {6, 10}, {3, 5}, {11, 1}, {5, 3}, {11, 9}, {3, 8}
+        };
+        System.out.println(Arrays.toString(solution(edges2)));
     }
 }
--- 2024_kakao_winter/주사위 고르기.java
+++ 2024_kakao_winter/주사위 고르기.java
@@ -2,68 +2,11 @@ import java.util.*;
 
 class GiftPrediction {
     public static void main(String[] args) {
-        String[] friends1 = { "muzi", "ryan", "frodo", "neo" };
-        String[] gifts1 = { "muzi frodo", "muzi frodo", "ryan muzi", "ryan muzi", "ryan muzi", "frodo muzi",
-                "frodo ryan", "neo muzi" };
-        System.out.println(solution(friends1, gifts1)); // Output: 2
-
-        String[] friends2 = { "joy", "brad", "alessandro", "conan", "david" };
-        String[] gifts2 = { "alessandro brad", "alessandro joy", "alessandro conan", "david alessandro",
-                "alessandro david" };
-        System.out.println(solution(friends2, gifts2)); // Output: 4
-
-        String[] friends3 = { "a", "b", "c" };
-        String[] gifts3 = { "a b", "b a", "c a", "a c", "a c", "c a" };
-        System.out.println(solution(friends3, gifts3)); // Output: 0
+        //아이디어만 생각하고 내일을 기약약
     }
 
-    public static int solution(String[] friends, String[] gifts) {
-        int n = friends.length;
-
-        Map<String, Map<String, Integer>> giftMap = new HashMap<>();
-        Map<String, Integer> givenGifts = new HashMap<>(), receivedGifts = new HashMap<>();
-        for (String f : friends) {
-            giftMap.put(f, new HashMap<>());
-            givenGifts.put(f, 0);
-            receivedGifts.put(f, 0);
-        }
-
-        for (String gift : gifts) {
-            String[] parts = gift.split(" ");
-            String giver = parts[0], receiver = parts[1];
-            giftMap.get(giver).put(receiver, giftMap.get(giver).getOrDefault(receiver, 0) + 1);
-            givenGifts.put(giver, givenGifts.get(giver) + 1);
-            receivedGifts.put(receiver, receivedGifts.get(receiver) + 1);
-        }
-
-        Map<String, Integer> giftScores = new HashMap<>();
-        for (String f : friends) {
-            giftScores.put(f, givenGifts.get(f) - receivedGifts.get(f));
-        }
-
-        Map<String, Integer> nextMonthGifts = new HashMap<>();
-        for (String f : friends)
-            nextMonthGifts.put(f, 0);
-
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                String f1 = friends[i], f2 = friends[j];
-                int f1ToF2 = giftMap.get(f1).getOrDefault(f2, 0);
-                int f2ToF1 = giftMap.get(f2).getOrDefault(f1, 0);
-                if (f1ToF2 > f2ToF1) {
-                    nextMonthGifts.put(f1, nextMonthGifts.get(f1) + 1);
-                } else if (f1ToF2 < f2ToF1) {
-                    nextMonthGifts.put(f2, nextMonthGifts.get(f2) + 1);
-                } else {
-                    if (giftScores.get(f1) > giftScores.get(f2)) {
-                        nextMonthGifts.put(f1, nextMonthGifts.get(f1) + 1);
-                    } else if (giftScores.get(f1) < giftScores.get(f2)) {
-                        nextMonthGifts.put(f2, nextMonthGifts.get(f2) + 1);
-                    }
-                }
-            }
-        }
-
-        return nextMonthGifts.values().stream().max(Integer::compare).orElse(0);
+    public int[] solution(int[][] dice) {
+        int[] answer = {};
+        return answer;
     }
 }
